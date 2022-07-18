960587e4ef1e93710bdef898cabad24f2d102c63
Rafael Cunha <rafacunha.prof033@gmail.com>
2022-07-18 01:14:12 +0000

    GetStaticPaths

--- pages/produtos/[id].js
+++ pages/produtos/[id].js
@@ -1,7 +1,34 @@
 // import { useRouter } from "next/router";
-export async function getServerSideProps(context) {
-    const id = context.query.id;
+// export async function getServerSideProps(context) {
+//     const id = context.query.id;
 
+//     return {
+//         props: {
+//             id: id
+//         }
+//     }
+// }
+
+export async function getStaticPaths() {
+    return {
+        paths: [{
+            params: {
+                id: '1'
+            }
+        },{
+            params: {
+                id: '2'
+            }
+        }],
+        // When false return 404 for any other page
+        fallback: 'blocking'
+    }
+}
+
+export async function getStaticProps(context) {
+    
+    const id = context.params.id;
+ 
     return {
         props: {
             id: id
